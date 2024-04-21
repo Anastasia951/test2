c3a478c72f9d604912c0d406fb9eb760c41c51cb
Anastasiya Ivanova <anastasiya.ivanova@vk.team>
2024-04-21 13:24:42 +0300

    init

---
+++ test2/Program.cs
@@ -0,0 +1,21 @@
+﻿try
+{
+    Console.Write("input a: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("input b: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+
+    if (a == b)
+    {
+        Console.WriteLine("a equals b");
+    } else if (a > b)
+    {
+        Console.WriteLine("a is more than b");
+    } else
+    {
+        Console.WriteLine("b is more than a");
+    }
+} catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
